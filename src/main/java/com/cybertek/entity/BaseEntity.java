6f2943adc3ed48e57b360dee3f046383c966610e
mustafa-ozkan <mustafa.ozkan1241@gmail.com>
2021-01-05 20:08:57 -0500

    update prePersist preUpdate

--- src/main/java/com/cybertek/entity/BaseEntity.java
+++ src/main/java/com/cybertek/entity/BaseEntity.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @NoArgsConstructor
@@ -25,5 +26,19 @@ public class BaseEntity {
     private Long lastUpdateUserId;
 
     private Boolean isDeleted = false;
+
+    @PrePersist
+    private void onPrePersist(){
+        this.insertDateTime= LocalDateTime.now();
+        this.lastUpdateDateTime =LocalDateTime.now();
+        this.insertUserId = 1L;
+        this.lastUpdateUserId=1L;
+    }
+
+    @PreUpdate
+    private void onPreUpdate(){
+        this.lastUpdateDateTime= LocalDateTime.now();
+        this.lastUpdateUserId=1L;
+    }
 
 }
