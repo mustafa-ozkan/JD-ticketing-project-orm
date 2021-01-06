cb0edf84c73e78453d2f24adc0d01ef7ee06e201
mustafa-ozkan <mustafa.ozkan1241@gmail.com>
2021-01-05 20:41:25 -0500

    update project classes

---
+++ src/main/java/com/cybertek/service/ProjectService.java
@@ -0,0 +1,13 @@
+package com.cybertek.service;
+
+import com.cybertek.dto.ProjectDTO;
+
+import java.util.List;
+
+public interface ProjectService {
+    ProjectDTO getByProjectCode(String projectCode);
+    List<ProjectDTO> listAllProjects();
+    ProjectDTO save(ProjectDTO projectDTO);
+    ProjectDTO update(ProjectDTO projectDTO);
+    void delete(String projectCode);
+}
